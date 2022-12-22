2ff548d5fa09d5e19b6896346d54e1a92e1f2c91
Frieder Singer <117061955+friedersinger@users.noreply.github.com>
2022-12-22 10:08:23 +0100

    Initial Commit

---
+++ project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project
+//
+//  Created by Frieder Singer on 22.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
